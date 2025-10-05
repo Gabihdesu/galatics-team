05e056937b55e3b0a15c62b4cb60813177b46b32
Gabihdesu <gabrielademelosantiago@outlook.com>
2025-10-05 03:07:43 -0300

    version-conlid

--- dist/app.py
+++ dist/app.py
@@ -4,12 +4,26 @@ app = Flask(__name__)
 
 # Rota principal: home/index
 @app.route('/')
-def welcome():
+def home():
     return render_template('home.html')
+
+@app.route('/video')
+def video():
+    return render_template('video.html')
 
 @app.route('/historia')
 def historia():
     return render_template('historia.html')
+
+@app.route('/simulador')
+def simulador():
+    return render_template('simulador.html')
+
+@app.route('/equipe')
+def equipe():
+    return render_template('equipe.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)  # debug=True é útil durante desenvolvimento
